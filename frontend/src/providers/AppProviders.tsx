b7a5897615aa26184071ac8c93c9ce817035ec14
adityaarakal <adityararakal@gmail.com>
2025-11-09 07:04:47 +0530

    Create global emotion cache in AppProviders

--- frontend/src/providers/AppProviders.tsx
+++ frontend/src/providers/AppProviders.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useMemo } from 'react'
+import { PropsWithChildren } from 'react'
 import { CacheProvider } from '@emotion/react'
 import createCache from '@emotion/cache'
 import { CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material'
@@ -6,17 +6,15 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../lib/queryClient'
 import { muiTheme } from '../theme'
 
-export const AppProviders = ({ children }: PropsWithChildren) => {
-  const emotionCache = useMemo(() => createCache({ key: 'mui', prepend: true }), [])
+const emotionCache = createCache({ key: 'mui', prepend: true })
 
-  return (
-    <CacheProvider value={emotionCache}>
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={muiTheme}>
-          <CssBaseline />
-          <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-        </ThemeProvider>
-      </StyledEngineProvider>
-    </CacheProvider>
-  )
-}
+export const AppProviders = ({ children }: PropsWithChildren) => (
+  <CacheProvider value={emotionCache}>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={muiTheme}>
+        <CssBaseline />
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  </CacheProvider>
+)
